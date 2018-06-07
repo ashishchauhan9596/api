c3fef094e63fb8b8c5bc71a8c7b958ff7c1759ba
Ashish Chauhan <test.chauhan.9596@gmail.com>
2018-06-07 13:01:39 +0530

    new modified

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php echo "hello"; 
 
 echo "bye";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+	<form>
+		<input type="text" name="test"><br>
+		<input type="password" name="tpass"><br>
+		<input type="submit" name="tsub" value="submit">
+	</form>
+</body>
+</html>
